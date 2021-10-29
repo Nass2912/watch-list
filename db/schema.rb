5c365bd54dbcb2d75e5e77e86be6affa484e81f0
AnasSoobratty <soobratty.as@gmail.com>
2021-10-29 12:29:35 +0400

    created seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,25 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_29_065151) do
+ActiveRecord::Schema.define(version: 2021_10_29_082527) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "bookmarks", force: :cascade do |t|
     t.string "comment"
+    t.bigint "movie_id", null: false
+    t.bigint "list_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.bigint "movies_id"
-    t.bigint "lists_id"
-    t.index ["lists_id"], name: "index_bookmarks_on_lists_id"
-    t.index ["movies_id"], name: "index_bookmarks_on_movies_id"
+    t.index ["list_id"], name: "index_bookmarks_on_list_id"
+    t.index ["movie_id"], name: "index_bookmarks_on_movie_id"
   end
 
   create_table "lists", force: :cascade do |t|
-    t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
   end
 
   create_table "movies", force: :cascade do |t|
@@ -40,9 +40,6 @@ ActiveRecord::Schema.define(version: 2021_10_29_065151) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "reviews", force: :cascade do |t|
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
+  add_foreign_key "bookmarks", "lists"
+  add_foreign_key "bookmarks", "movies"
 end
